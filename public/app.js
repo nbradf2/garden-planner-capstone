d94f52667d25b30a74566d75d0499390a7c78d19
nbradf2 <nbradf2@gmail.com>
2017-11-14 19:19:54 -0600

    add mock data to app.js

---
+++ public/app.js
@@ -0,0 +1,39 @@
+var MOCK_GARDEN_LIST = {
+	"list": [
+		{
+			"id": "1111111",
+			"name": "Heirloom Tomato",
+			"start-date": "April 20, 2018",
+			"harvest-date": "July 20, 2018"
+			"comments": "Use crushed eggshells to supplement"
+		},
+		{
+			"id": "2222222",
+			"name": "Carrots",
+			"start-date": "April 4, 2018",
+			"harvest-date": "July 10, 2018"
+			"comments": "Plant with rosemary"
+		},
+		{
+			"id": "3333333",
+			"name": "Basil",
+			"start-date": "May 29, 2018",
+			"harvest-date": "July 30, 2018"
+			"comments": "Good companion for tomatoes"
+		},
+		{
+			"id": "4444444",
+			"name": "Lavendar",
+			"start-date": "May 5, 2018",
+			"harvest-date": "August 1, 2018"
+			"comments": "Hard to start from seed"
+		},
+		{
+			"id": "5555555",
+			"name": "Peas",
+			"start-date": "April 4, 2018",
+			"harvest-date": "July 20, 2018"
+			"comments": "Likes cooler weather"
+		}
+	]
+}
