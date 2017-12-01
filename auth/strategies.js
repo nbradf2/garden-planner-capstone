aa1c6a265d9c29a00453a82afc06986a102eaaa2
nbradf2 <nbradf2@gmail.com>
2017-11-30 18:53:02 -0600

    add JWT username/password retrieval to auth/strategies.js

---
+++ auth/strategies.js
@@ -0,0 +1,72 @@
+// allow users to provide credentials in order to access endpoints
+// using passport.js to set up a basic authentication strategy:
+// - retrieve UN and pass from request's Auth header
+// - check if they are valid
+// - use the strategy to protect the /api/auth/login endpoint
+
+const passport = require('passport');
+const {BasicStrategy} = require('passport-http');
+const {
+	// assignes Strategy export to name JwtStrategy using object destructuring:
+	Strategy: JwtStrategy,
+	ExtractJwt
+} = require('passport-jwt');
+
+const {User} = require('../users/models');
+const {JWT_SECRET} = require('../config');
+
+// BASIC authentication strategy (to use, register in server.js):
+const basicStrategy = new BasicStrategy((username, password, callback) => {
+	let user;
+	// look for a user with the supplied username:
+	User.findOne({username: username})
+		.then(_user => {
+			user = _user;
+			if(!user) {
+				// return rejected promise to break out of chain of .thens
+				return Promise.reject({
+					reason: 'LoginError',
+					message: 'Incorrect username or password'
+				});
+			}
+			// If the user is found, we then call the validate Password method
+			return user.validatePassword(password);
+		})
+		.then(isValid => {
+			if(!isValid) {
+				return Promise.reject({
+					reason: 'LoginError', 
+					message: 'Incorrect username or password'
+				});
+			}
+			return callback(null, user);
+		})
+		.catch(err => {
+			if (err.reason === 'LoginError') {
+				return callback(null, false, err);
+			}
+			return callback(err, false);
+		});
+});
+
+// After user has their JWT, it needs to access an endpoint:
+const jwtStrategy = new JwtStrategy(
+	{
+		// pass the same secret key that was used to sign the tokens
+		// specifying that we will only allow tokens signed with HS256:
+		secretOrKey: JWT_SECRET,
+		// look for JWT as a Bearer auth header:
+		jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+		// only allow HS256 tokens - the same as the ones we issue:
+		algorithms: ['HS256']
+	},
+	// if a valid JWT is supplied, this callback will be run indicating
+	// we have authenticated successfully:
+	(payload, done) => {
+		done(null, payload.user);
+
+		// to register this JWT strategy with Passport, use the passport.use method in server.js
+	}
+);
+
+module.exports = {basicStrategy, jwtStrategy};
